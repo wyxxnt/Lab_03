182186c03a6875e62ce04f2cc02bb4f022b26c1f
wyxxnt <212143260+wyxxnt@users.noreply.github.com>
2025-10-08 07:40:12 -0400

    виправив функцію random

--- random_numbers.py
+++ random_numbers.py
@@ -1,16 +1,16 @@
 import random
+import math
 
-def my_random(a, b):
-    x = random.random()
-    result = x * (b - a) + a
-    return int(result)
+def random_func(min_val=None, max_val=None):
+    if max_val is None and min_val is not None:
+        max_val = min_val
+        min_val = 0
+    elif min_val is None and max_val is None:
+        return random.random()
+    
+    result = random.random() * (max_val - min_val) + min_val
+    return math.floor(result)
 
-def my_random_max(max_num):
-    x = random.random()
-    result = x * max_num
-    return int(result)
-
-print(my_random(1, 10))
-print(my_random_max(50))
-print(my_random(10, 20))
-print("тест:", my_random(5, 15))
+print(random_func(5, 15))
+print(random_func(20))
+print(random_func(1, 100))
