3488f7fde042cc5d704036927f708c9d16426333
Kyrylo <212143260+wyxxnt@users.noreply.github.com>
2025-10-08 15:07:37 +0300

    Update key_generator.py

--- key_generator.py
+++ key_generator.py
@@ -1,14 +1,17 @@
 import random
 
-def generateKey(length, characters):
+def make_key(size, chars):
     key = ""
     i = 0
-    while i < length:
-        num = random.randint(0, len(characters) - 1)
-        key = key + characters[num]
+    while i < size:
+        num = random.randint(0, len(chars) - 1)
+        key = key + chars[num]
         i = i + 1
     return key
 
-characters = "abcdefghijklmnopqrstuvwxyz0123456789"
-key = generateKey(16, characters)
-print(key)
+letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+my_key = make_key(16, letters)
+print(my_key)
+
+short_key = make_key(8, letters)
+print(short_key)
